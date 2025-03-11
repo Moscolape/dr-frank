f6221baae14ff346cb008662f3bb986aa0d59328
Moscolape <chunkums088@gmail.com>
2025-03-11 07:52:35 +0100

    chore: update marquee slider component

--- src/components/marqueeSlider.tsx
+++ src/components/marqueeSlider.tsx
@@ -1,47 +1,23 @@
-import { motion } from "framer-motion";
-import { useEffect, useState, useRef } from "react";
+import Marquee from "react-fast-marquee";
 import { Image1, Image10, Image11, Image12, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9 } from "../constants/assets";
-
 
 const images = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12];
 
 
 const MarqueeSlider = () => {
-  const [speed, setSpeed] = useState(40);
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const updateSpeed = () => {
-      const screenWidth = window.innerWidth;
-      setSpeed(screenWidth < 768 ? 60 : 40);
-    };
-
-    updateSpeed();
-    window.addEventListener("resize", updateSpeed);
-    return () => window.removeEventListener("resize", updateSpeed);
-  }, []);
-
   return (
-    <div ref={containerRef} className="overflow-hidden w-full py-5 my-5">
-      <motion.div
-        className="flex space-x-6 w-max"
-        animate={{ x: ["0%", "-100%"] }}
-        transition={{
-          ease: "linear",
-          duration: speed,
-          repeat: Infinity,
-        }}
-      >
-        {[...images, ...images].map((src, index) => (
+    <Marquee className="overflow-hidden w-full py-5 my-5">
+      <div className="flex items-center">
+        {images.map((src, index) => (
           <img
             key={index}
             src={src}
             alt={`Slide ${index}`}
-            className="h-44 w-auto object-cover"
+            className="h-32 w-auto object-cover mr-5"
           />
         ))}
-      </motion.div>
-    </div>
+      </div>
+    </Marquee>
   );
 };
 
